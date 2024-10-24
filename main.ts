a6d7a7b97b5a31f2567aad5325e820fe30bf44ba
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2024-10-23 17:35:43 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . c c c c c . . . . . . . 
+    . . . c d d d d d c . . . . . . 
+    . . . c d c c c c c . . . . . . 
+    . . . c c d 4 4 4 4 c . . . . . 
+    . . . c 1 4 4 4 4 4 d c . . . . 
+    . . c 1 4 4 4 4 4 1 4 4 c . . . 
+    c c c 1 4 4 4 4 1 4 4 4 4 c . . 
+    c 4 4 1 4 4 c c 1 4 4 4 4 4 c . 
+    f 4 f 1 4 f 4 4 1 4 4 4 4 4 c . 
+    f 4 f d 4 f 4 c 1 4 f 4 4 4 4 f 
+    f 4 f f 4 f f 4 1 4 4 4 4 4 4 f 
+    f f c b c 4 4 4 4 1 4 4 4 4 f . 
+    . . c d d c 4 4 4 4 d f f f . . 
+    . . . c c c f f f f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
